24eaf8d822185c8531f91c74a71f2cbd1eb9266d
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-28 01:17:44 -0700

    adds docker + init + readme

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+TaoHash Proxy - Mining proxy for TaoHash subnet.
+
+This package provides the core implementation of the TaoHash mining proxy, 
+which serves as an intermediary between miners and mining pools.
+"""
