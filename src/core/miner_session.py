f9b24e5ccdc5f73d80349ad04347b5299ec65b06
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-07-03 17:11:08 -0700

    slightly increase auth timeout

--- src/core/miner_session.py
+++ src/core/miner_session.py
@@ -103,7 +103,7 @@ class MinerSession:
         has_authorize = False
         authorize_time = None
         
-        AUTHORIZE_TIMEOUT = 0.3
+        AUTHORIZE_TIMEOUT = 0.4
         POST_AUTHORIZE_WAIT = 0.2
         
         start_time = time.time()
